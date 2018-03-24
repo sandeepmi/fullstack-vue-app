dd13d0cc8b269bc4da680aaf9655ef6198fdaf89
Sandeep M <sandeepmi@outlook.com>
2018-03-23 23:52:52 -0500

    design changes

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,28 +1,10 @@
 <template>
   <div class="container page-home">
-    <section class="mt-3">
-      <div class="row">
+    <section>
+      <div class="row text-center">
         <div class="col-md">
           <h1>Home</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-        <div class="col-md">
-          <div class="card">
-            <div class="card-image">
-              <img class="img-fluid" src="http://via.placeholder.com/800x200">
-            </div>
-            <div class="card-body">
-              <div class="card-title"><strong>Card title</strong></div>
-              <div class="card-text">
-                <p>I am a very simple card. I am good at containing small bits of information.
-                I am convenient because I require little markup to use effectively.</p>
-              </div>
-              <div class="card-action">
-                <a @click="showLinkInfo">This is a link</a>
-              </div>
-            </div>
-          </div>
+          <h2>Lorem ipsum dolor sit amet, <br />consectetur adipiscing elit, sed do eiusmod tempor</h2>
         </div>
       </div>
     </section>
@@ -30,29 +12,29 @@
     <section>
       <div class="row">
         <div class="col-md">
-          <div class="card">
+          <div class="card card-transparent">
             <div class="card-body text-center">
               <i class="material-icons medium">insert_emoticon</i>
               <h5>Smiley</h5>
-              <p class="light center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <p class="light center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
             </div>
           </div>
         </div>
         <div class="col-md">
-          <div class="card">
+          <div class="card card-transparent">
             <div class="card-body text-center">
               <i class="material-icons medium">insert_emoticon</i>
               <h5>Smiley</h5>
-              <p class="light center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <p class="light center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
             </div>
           </div>
         </div>
         <div class="col-md">
-          <div class="card">
+          <div class="card card-transparent">
             <div class="card-body text-center">
               <i class="material-icons medium">insert_emoticon</i>
               <h5>Smiley</h5>
-              <p class="light center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <p class="light center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
             </div>
           </div>
         </div>
